93b89dba98256a6827ffa74fd2f2d3bf41bb9827
aparkerjones <parkor.j@gmail.com>
2024-06-05 21:43:59 -0600

    Finished Prep4

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,18 @@
+using System;
+
+class MathAssignment : Assignment
+{
+    string _textbookSection;
+    string _problems;
+
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems)
+        : base(studentName, topic)
+    {
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+     public string GetHomeworkList()
+     {
+            return $"Section {_textbookSection} Probelms {_problems}";
+     }
+}
