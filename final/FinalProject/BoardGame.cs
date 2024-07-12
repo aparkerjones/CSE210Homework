44727864a58877bb1056353579c3089bc7fff114
aparkerjones <parkor.j@gmail.com>
2024-07-11 21:51:08 -0600

    Final Project Empty Shell

---
+++ final/FinalProject/BoardGame.cs
@@ -0,0 +1,8 @@
+using System;
+
+class BoardGame: Item
+{
+    int maxplayers;
+    int minplayers;
+    
+}
---
+++ final/FinalProject/Item.cs
@@ -0,0 +1,30 @@
+using System;
+
+
+    class Item
+    {
+        string name;
+        int agemin;
+        string genre;
+
+        private List<Item> loadlibrary()
+        {
+            
+        }
+        private void savelibrary()
+        {
+
+        }
+        private newitem()
+        {
+
+        }
+        private removeitem()
+        {
+
+        }
+        private printlibrary()
+        {
+            
+        }
+    }
