c252a41f15795b5115f10d766ef513f1947bd2d1
aparkerjones <parkor.j@gmail.com>
2024-07-14 17:10:57 -0600

    Unit 4 40% complete

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Activity
+{
+    int duration;
+
+    public Activity()
+    {
+        
+    }
+
+}
---
+++ prove/Develop04/Breathing.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Breathing : Activity
+{
+    int repetitions;
+    public Breathing()
+    {
+        
+    }
+}
